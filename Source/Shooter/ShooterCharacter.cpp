abf731ba317ed80f98f5944bbc03da3bae623f3d
emrahbolum <emrahbolum@gmail.com>
2022-10-24 13:17:58 +0300

    Update ShooterCharacter.cpp

--- Source/Shooter/ShooterCharacter.cpp
+++ Source/Shooter/ShooterCharacter.cpp
@@ -15,7 +15,7 @@ AShooterCharacter::AShooterCharacter()
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	OutputLog();
+	//OutputLog();
 }
 
 // Called every frame
@@ -39,9 +39,30 @@ void AShooterCharacter::OutputLog()
 	//Normal text
 	UE_LOG(LogTemp, Warning, TEXT("Normal text"))
 
-	int MyInt{ 32 }; //süslü parantezde yapmamýzýn sebebi hatayý önlemektir. Örneðin int tanýmlayýp int MyInt {33.4} yaparsak hata verecektir.
+		int MyInt{ 32 }; //süslü parantezde yapmamýzýn sebebi hatayý önlemektir. Örneðin int tanýmlayýp int MyInt {33.4} yaparsak hata verecektir.
 	UE_LOG(LogTemp, Warning, TEXT("MyInt value: %d"), MyInt)
 
+		float MyFloat{ 3.14159f }; //f demezsek double algýlar ve bellekte fazla yer kaplar
+	UE_LOG(LogTemp, Warning, TEXT("MyFloat value: %f"), MyFloat)
 
+		double MyDouble{ 0.0000043 };
+	UE_LOG(LogTemp, Warning, TEXT("MyDouble value: %lf"), MyDouble) // %lf double için kullanýlýyor
 
+		char MyChar{ 'a' };
+	UE_LOG(LogTemp, Warning, TEXT("MyChar value: %c"), MyChar)	// %c char için kullanýlýyor
+
+		wchar_t wide_ch { L'a' };
+	UE_LOG(LogTemp, Warning, TEXT("MyWideChar value: %lc"), wide_ch) // %lc widechar (wchar) için kullanýlýyor.
+
+		bool MyBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("MyBool value: %d"), MyBool)
+
+		UE_LOG(LogTemp, Warning, TEXT("%d %f %d"), MyInt, MyFloat, MyBool)
+
+		FString MyString {
+		TEXT("This is FString baby!..")
+	};
+
+	UE_LOG(LogTemp, Warning, TEXT("FString value: %s"), *MyString); // %s ile gösterilir. FStringte aþýrý yükleme olduðu için baþa * iþareti koyuyoruz.
+	UE_LOG(LogTemp, Warning, TEXT("GetName Islemi: %s"), *GetName());
 }
