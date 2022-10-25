f3cdea650839ff45830a8cac98a099cb87927de9
emrahbolum <emrahbolum@gmail.com>
2022-10-25 14:23:48 +0300

    Added AnimBp

--- Source/Shooter/ShooterAnimInstance.cpp
+++ Source/Shooter/ShooterAnimInstance.cpp
@@ -2,4 +2,49 @@
 
 
 #include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	//Karakter geçerli deðil ise
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
 
+	//Karakter geçerli ise
+	if (ShooterCharacter)
+	{
+		//HIZI HESAPLA
+		//Yanal hýzý hesaplayalým
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		//Hýzýn yukarý-aþaðý (Z) yönünü sýfýrlamamýz gerekiyor yoksa yukarýdan aþaðý düþerken
+		//ya da zýplarken hýz deðiþir
+		Velocity.Z = 0.f;
+		Speed = Velocity.Size();	//Vektörü float deðere dönüþtürmüþ olduk.
+
+		//HAVADA MI?
+		//#include "GameFramework/CharacterMovementComponent.h"
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();	//CharacterMovement.h ý dahil etmemiz gerekiyor.
+
+		//HAREKET HALÝNDE MÝ?
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0)
+		{//Vektör döndürdüðü için GetCurrentAcceleration dedikten sonra .Size dedik	 
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+			//DÝPNOT: Normalde ivme 0 ken hýz 0 dan farklý olabilir. Ama burda kastedilen ivme karakterin hareket halinde olup olmadýðýdýr.	
+		}
+		
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	//ShooterCharacter pointerý boþ. Ýçini dolduruyoruz.
+	//TryGetPawnOwner diyerek kullanýlan pawný Shoter karaktere döküyoruz.
+	ShooterCharacter =Cast<AShooterCharacter> (TryGetPawnOwner());
+
+}
--- Source/Shooter/ShooterAnimInstance.h
+++ Source/Shooter/ShooterAnimInstance.h
@@ -13,5 +13,24 @@ UCLASS()
 class SHOOTER_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+
+	//Blueprintten çaðýrýlabilir.
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	//Sanal iþlevi devre dýþý býrakýyoruz. Override ediyoruz
+	//Bu aktor beginplayi gibidir. Üyeler burada baþlatýlýr.
+	virtual void NativeInitializeAnimation() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;	//Hýz	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;	//Havada mý?
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;	//Karakter hareket halinde mi?
 };
